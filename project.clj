a2a2f2d24d4a9ee67eb7fe1bd17aaf0a974fcc6e
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2012-05-19 18:00:49 +0200

    "Upgraded ring and included hiccup"

--- project.clj
+++ project.clj
@@ -1,14 +1,15 @@
 (defproject ttmachines "0.1.0-SNAPSHOT"
   :description "Teaching beginners to program using a ClojureScript REPL."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ring "1.0.2"]
+                 [ring "1.1.0"]
                  [compojure "1.0.1"]
                  [ring-clj-params "0.1.0"]
                  [org.clojure/clojurescript "0.0-971"]
                  [com.google.javascript/closure-compiler "r1592"]
                  [org.clojure/google-closure-library "0.0-790"]
                  [org.mozilla/rhino "1.7R3"]
-                 [cheshire "4.0.0"]]
+                 [cheshire "4.0.0"]
+                 [hiccup "1.0.0"]]
   :plugins [[lein-cljsbuild "0.1.2"]]
   :dev-dependencies [[jline "0.9.94"]
                      [lein-marginalia "0.7.0-SNAPSHOT"]]
@@ -18,7 +19,7 @@
                :jar true,
                :compiler
                {:output-dir "resources/public/javascript/cljs",
-                :output-to "resources/public/javascript/repl.js",
+                :output-to "resources/public/javascript/ttmachines.js",
                 :optimizations :simple,
                 :repl-listen-port 9000,
                 :pretty-print true}}]}
