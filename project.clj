6422b5e5dd1e86e603bccc74284d157cb3723c5b
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2012-05-25 23:04:39 +0200

    Include goog.jar, clj and cljs source on the classpath

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
                  [org.clojure/clojurescript "0.0-971"]
                  [com.google.javascript/closure-compiler "r1592"]
                  [org.clojure/google-closure-library "0.0-790"]
+                 [goog-jar "1.0.0"]
                  [org.mozilla/rhino "1.7R3"]
                  [hiccup "1.0.0"]
                  [markdown-clj "0.8"]]
@@ -13,11 +14,6 @@
   :dev-dependencies [[jline "0.9.94"]
                      [lein-marginalia "0.7.0-SNAPSHOT"]
                      [lein-git-deps "0.0.1-SNAPSHOT"]]
-  :git-dependencies [["https://github.com/levand/domina.git"
-                      "8933b2d12c44832c9bfaecf457a1bc5db251a774"]]
-  :extra-classpath-dirs [".lein-git-deps/domina/src/cljs"
-                         "src/clj"
-                         "src/cljs"]
   :cljsbuild {
               :builds
               [{:source-path "src/cljs",
@@ -28,8 +24,10 @@
                 :optimizations :simple,
                 :repl-listen-port 9000,
                 :pretty-print true
-                :libs [".lein-git-deps/domina/src/cljs"]}}]}
+                :libs ["goog/dom/query.js"]}}]}
   :jvm-opts ["-Djava.security.policy=heroku.policy" "-Xmx80M"]
   :source-path "src/clj"
-  :main ttmachines.server.app)
-
+  :extra-classpath-dirs ["src/clj"
+                         "src/cljs"]
+  :main ttmachines.server.app
+  :repl-init ttmachines.server.tools)
